184cbeb448c54abf87fead7eb0a90bd0898e99c6
rebelzach <zach@relevantwalk.com>
2012-06-04 14:10:51 -0600

    Added debug macros to the speed indicator

--- ArudinoTempoBox/ArudinoTempoBox.ino
+++ ArudinoTempoBox/ArudinoTempoBox.ino
@@ -13,19 +13,21 @@ TempoAppController appController;
 void setup() 
 { 
   appController.initialize();
-
+#if DEBUG 
   loopStartTime = millis();
+#endif
 }
 
 void loop() 
 {
   appController.processLoop();
-  
+#if DEBUG 
   loopCounter++;
   if (millis() - loopStartTime > 10000) {
-    debugPrint("Loop: ");
+    debugPrint("Loop count per second: ");
     debugPrintln(loopCounter/10);
     loopStartTime = millis();
     loopCounter = 0;
   }
+#endif
 }
